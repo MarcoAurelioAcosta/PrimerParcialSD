e2e8300100f0af6907df923e3ba0040d604ca2bf
MarcoAurelioAcosta <marcochongsu@gmail.com>
2021-11-27 10:57:04 -0300

    Modificacion para el primer examen Final

--- HolaMundo.c
+++ HolaMundo.c
@@ -7,5 +7,7 @@ int main()
 {
     printf( "Hola,esta es una practica de impresion en lenguaje c" );
     printf("Modificacion para el primer parcial,nueva impresion");
+
+     printf("Modificacion para el primer examen final,nueva impresion, Marco Aurelio Acosta Posadas, 27 de Noviembre de 2021");
     return 0;
 }
